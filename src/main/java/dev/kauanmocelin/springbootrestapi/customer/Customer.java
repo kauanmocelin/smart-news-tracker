d5144bd76d095fa396d7f4310c3accec573b62ff
kauanmocelin <kauanmocelin@gmail.com>
2021-07-23 17:08:29 -0300

    Mapped Customer entity to JPA

--- src/main/java/dev/kauanmocelin/springbootrestapi/customer/Customer.java
+++ src/main/java/dev/kauanmocelin/springbootrestapi/customer/Customer.java
@@ -1,16 +1,23 @@
 package dev.kauanmocelin.springbootrestapi.customer;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
-@NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@ToString
+@Data
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private LocalDate dateOfBirth;
 }
+
