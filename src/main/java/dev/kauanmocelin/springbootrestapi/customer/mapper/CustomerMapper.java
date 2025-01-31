3125d1243669598c7ceffb8bf2e95b2199898fba
Kauan Mocelin <kauanmocelin@gmail.com>
2025-01-31 20:46:32 -0300

    update mapstruct and ignore some attributes mapping

--- src/main/java/dev/kauanmocelin/springbootrestapi/customer/mapper/CustomerMapper.java
+++ src/main/java/dev/kauanmocelin/springbootrestapi/customer/mapper/CustomerMapper.java
@@ -4,10 +4,14 @@ import dev.kauanmocelin.springbootrestapi.customer.Customer;
 import dev.kauanmocelin.springbootrestapi.customer.request.CustomerPostRequestBody;
 import dev.kauanmocelin.springbootrestapi.customer.request.CustomerPutRequestBody;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "age", ignore = true)
     Customer toCustomer(CustomerPostRequestBody customerPostRequestBody);
 
+    @Mapping(target = "age", ignore = true)
     Customer toCustomer(CustomerPutRequestBody customerPutRequestBody);
 }
