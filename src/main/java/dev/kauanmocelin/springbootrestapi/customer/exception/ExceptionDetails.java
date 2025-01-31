43bdd46afebab890de2701917c3c84c44bf217d3
Kauan Mocelin <kauanmocelin@gmail.com>
2025-01-31 19:42:52 -0300

    adjusts

--- src/main/java/dev/kauanmocelin/springbootrestapi/customer/exception/ExceptionDetails.java
+++ src/main/java/dev/kauanmocelin/springbootrestapi/customer/exception/ExceptionDetails.java
@@ -1,15 +1,14 @@
 package dev.kauanmocelin.springbootrestapi.customer.exception;
 
+import java.time.LocalDateTime;
+
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
-
-import java.time.LocalDateTime;
 
 @Data
 @SuperBuilder
 public class ExceptionDetails {
     protected String title;
-    protected int status;
     protected String details;
     protected String developerMessage;
     protected LocalDateTime timestamp;
