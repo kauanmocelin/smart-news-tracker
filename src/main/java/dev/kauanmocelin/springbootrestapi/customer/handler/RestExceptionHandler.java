34f3455cca2d113e8dc950940323b92ffb052ecc
Kauan Mocelin <kauanmocelin@gmail.com>
2025-01-31 19:39:03 -0300

    Convert to RestControllerAdvice

--- src/main/java/dev/kauanmocelin/springbootrestapi/customer/handler/RestExceptionHandler.java
+++ src/main/java/dev/kauanmocelin/springbootrestapi/customer/handler/RestExceptionHandler.java
@@ -1,35 +1,36 @@
 package dev.kauanmocelin.springbootrestapi.customer.handler;
 
-import dev.kauanmocelin.springbootrestapi.customer.exception.BadRequestException;
-import dev.kauanmocelin.springbootrestapi.customer.exception.BadRequestExceptionDetails;
-import dev.kauanmocelin.springbootrestapi.customer.exception.FieldErrorMessage;
-import dev.kauanmocelin.springbootrestapi.customer.exception.ValidationExceptionDetails;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
+import dev.kauanmocelin.springbootrestapi.customer.exception.BadRequestException;
+import dev.kauanmocelin.springbootrestapi.customer.exception.BadRequestExceptionDetails;
+import dev.kauanmocelin.springbootrestapi.customer.exception.FieldErrorMessage;
+import dev.kauanmocelin.springbootrestapi.customer.exception.ValidationExceptionDetails;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException bre) {
-        return new ResponseEntity<>(
-                BadRequestExceptionDetails.builder()
-                        .timestamp(LocalDateTime.now())
-                        .status(HttpStatus.BAD_REQUEST.value())
-                        .title("Bad Request Exception, check de documentation")
-                        .details(bre.getMessage())
-                        .developerMessage(bre.getClass().getName())
-                        .build(), HttpStatus.BAD_REQUEST);
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public BadRequestExceptionDetails handleBadRequestException(BadRequestException bre) {
+        return BadRequestExceptionDetails.builder()
+		        .timestamp(LocalDateTime.now())
+		        .title("Bad Request Exception, check de documentation")
+		        .details(bre.getMessage())
+		        .developerMessage(bre.getClass().getName())
+		        .build();
     }
 
     @Override
@@ -43,12 +44,11 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
         return new ResponseEntity<>(
                 ValidationExceptionDetails.builder()
-                        .timestamp(LocalDateTime.now())
-                        .status(HttpStatus.BAD_REQUEST.value())
-                        .title("Bad Request Exception, check de documentation")
-                        .details("Check the field(s) error")
-                        .developerMessage(exception.getClass().getName())
-                        .fieldsErrorValidation(fieldsErrorMessage)
-                        .build(), HttpStatus.BAD_REQUEST);
+	                    .timestamp(LocalDateTime.now())
+	                    .title("Bad Request Exception, check de documentation")
+	                    .details("Check the field(s) error")
+	                    .developerMessage(exception.getClass().getName())
+	                    .fieldsErrorValidation(fieldsErrorMessage)
+	                    .build(), HttpStatus.BAD_REQUEST);
     }
 }
