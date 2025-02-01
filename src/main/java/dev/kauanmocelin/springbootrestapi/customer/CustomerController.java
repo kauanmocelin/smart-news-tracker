76a0d58dc1d0d11921f8ce6e6d1df744d4dc535c
Kauan Mocelin <kauanmocelin@gmail.com>
2025-02-01 20:57:42 -0300

    fix resource naming

--- src/main/java/dev/kauanmocelin/springbootrestapi/customer/CustomerController.java
+++ src/main/java/dev/kauanmocelin/springbootrestapi/customer/CustomerController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController()
-@RequestMapping(path = "api/v1/customer")
+@RequestMapping(path = "api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
 
