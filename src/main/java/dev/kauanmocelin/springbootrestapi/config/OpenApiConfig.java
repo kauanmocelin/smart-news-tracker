588be2f7a3f27e2fab74042bcab7ae37f33fd261
Kauan Mocelin <kauanmocelin@gmail.com>
2025-02-10 14:32:20 -0300

    add description and security layer to swagger

---
+++ src/main/java/dev/kauanmocelin/springbootrestapi/config/OpenApiConfig.java
@@ -0,0 +1,53 @@
+package dev.kauanmocelin.springbootrestapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(
+        contact = @Contact(
+            name = "Kauan",
+            email = "kauan@gmail.com",
+            url = "http://kauan.dev"
+        ),
+        description = "OpenApi documentation for Rest Api Springboot",
+        title = "OpenApi specification - Rest Api",
+        version = "1.0",
+        license = @License(
+            name = "MIT",
+            url = "https://github.com/kauanmocelin/rest-api-springboot/blob/main/LICENSE"
+        ),
+        termsOfService = "http://Terms-of-service"
+    ),
+    servers = {
+        @Server(
+            description = "Local ENV",
+            url = "http://localhost:8080"
+        ),
+        @Server(
+            description = "Local PROD",
+            url = "http://kauan-rest-api-springboot.dev"
+        )
+    },
+    security = {
+        @SecurityRequirement(
+            name = "basicAuth"
+        )
+    }
+)
+@SecurityScheme(
+    name = "basicAuth",
+    description = "Basic auth description",
+    scheme = "basic",
+    type = SecuritySchemeType.HTTP,
+    in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
