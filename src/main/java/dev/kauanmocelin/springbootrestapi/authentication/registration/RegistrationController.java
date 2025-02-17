122eb429ff204e36ce942c157c6074897e304122
Kauan Mocelin <kauanmocelin@gmail.com>
2025-02-17 09:59:05 -0300

    api swagger documentation for registration flow

--- src/main/java/dev/kauanmocelin/springbootrestapi/authentication/registration/RegistrationController.java
+++ src/main/java/dev/kauanmocelin/springbootrestapi/authentication/registration/RegistrationController.java
@@ -3,6 +3,9 @@ package dev.kauanmocelin.springbootrestapi.authentication.registration;
 import dev.kauanmocelin.springbootrestapi.authentication.registration.request.LoginRequest;
 import dev.kauanmocelin.springbootrestapi.authentication.registration.request.LoginResponse;
 import dev.kauanmocelin.springbootrestapi.authentication.registration.request.RegistrationRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +20,14 @@ public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping("/register")
-//    @Operation(summary = "Register new customer", description = "Register new customer", tags = {"customer"})
+    @Operation(summary = "Register new user", description = "Register new user", tags = {"authentication"})
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "User successfully registered. A verification email has been sent."),
+        @ApiResponse(responseCode = "400", description = "User email already has been used")
+    })
     public ResponseEntity<String> register(@RequestBody @Valid RegistrationRequest request) {
         final var registrationCode = registrationService.register(request);
-        final var uri =  ServletUriComponentsBuilder
+        final var uri = ServletUriComponentsBuilder
             .fromCurrentContextPath()
             .replacePath("/api/v1/auth/verify")
             .queryParam("token", registrationCode)
@@ -29,11 +36,21 @@ public class RegistrationController {
         return ResponseEntity.created(uri).body(registrationCode);
     }
 
+    @Operation(summary = "Verify confirmation code", description = "Verify confirmation codeVerify confirmation code", tags = {"authentication"})
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Confirmation code successfully verified"),
+        @ApiResponse(responseCode = "400", description = "Possible reasons: confirmation code not found, account already confirmed or confirmation code expired"),
+    })
     @GetMapping(path = "/verify")
     public String confirm(@RequestParam("token") String token) {
         return registrationService.confirmToken(token);
     }
 
+    @Operation(summary = "User login", description = "User login", tags = {"authentication"})
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User logged in successfully"),
+        @ApiResponse(responseCode = "403", description = "Invalid credentials")
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> authenticate(@RequestBody @Valid LoginRequest request) {
         return ResponseEntity.ok(registrationService.login(request));
