21db1465e7af96e873a0de903ae4be9e69225c27
Kauan Mocelin <kauanmocelin@gmail.com>
2025-02-16 22:04:29 -0300

    test web layer excluding jwt filter

--- src/test/java/dev/kauanmocelin/springbootrestapi/news/NewsMonitorControllerTest.java
+++ src/test/java/dev/kauanmocelin/springbootrestapi/news/NewsMonitorControllerTest.java
@@ -2,14 +2,14 @@ package dev.kauanmocelin.springbootrestapi.news;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.kauanmocelin.springbootrestapi.common.security.JwtAuthenticationFilter;
-import dev.kauanmocelin.springbootrestapi.common.security.JwtService;
 import dev.kauanmocelin.springbootrestapi.news.request.KeywordRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,10 +18,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(
-    controllers = NewsMonitorController.class,
-    excludeAutoConfiguration = SecurityAutoConfiguration.class
-)
-@AutoConfigureMockMvc(addFilters = false)
+    value = NewsMonitorController.class,
+    excludeAutoConfiguration = SecurityAutoConfiguration.class,
+    excludeFilters =
+    @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = JwtAuthenticationFilter.class))
 class NewsMonitorControllerTest {
 
     @Autowired
@@ -29,12 +31,6 @@ class NewsMonitorControllerTest {
 
     @MockitoBean
     private NewsMonitorService newsMonitorService;
-
-    @MockitoBean
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    @MockitoBean
-    private JwtService jwtService;
 
     @Test
     @DisplayName("Should return status created when register new keyword")
