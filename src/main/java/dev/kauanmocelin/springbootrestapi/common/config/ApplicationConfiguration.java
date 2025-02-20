a1e5d55b788a6a207b5c2d89f299fc87a1b7832c
Kauan Mocelin <kauanmocelin@gmail.com>
2025-02-20 13:44:32 -0300

    rename files

--- src/main/java/dev/kauanmocelin/springbootrestapi/common/config/ApplicationConfiguration.java
+++ src/main/java/dev/kauanmocelin/springbootrestapi/common/config/ApplicationConfiguration.java
@@ -1,11 +1,13 @@
 package dev.kauanmocelin.springbootrestapi.common.config;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class ApplicationConfig {
+@ConfigurationPropertiesScan("dev.kauanmocelin.springbootrestapi")
+public class ApplicationConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
--- src/main/java/dev/kauanmocelin/springbootrestapi/common/config/OpenApiConfiguration.java
+++ src/main/java/dev/kauanmocelin/springbootrestapi/common/config/OpenApiConfiguration.java
@@ -49,5 +49,5 @@ import io.swagger.v3.oas.annotations.servers.Server;
     type = SecuritySchemeType.HTTP,
     in = SecuritySchemeIn.HEADER
 )
-public class OpenApiConfig {
+public class OpenApiConfiguration {
 }
