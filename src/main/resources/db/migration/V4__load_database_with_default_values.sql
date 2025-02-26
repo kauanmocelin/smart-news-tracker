92003535ad5c35ed0441025669bcdbfb01df306e
Kauan Mocelin <kauanmocelin@gmail.com>
2025-02-26 11:39:59 -0300

    change role default user to admin

--- src/main/resources/db/migration/V4__load_database_with_default_values.sql
+++ src/main/resources/db/migration/V4__load_database_with_default_values.sql
@@ -6,4 +6,4 @@ INSERT INTO app_users (first_name, last_name, email, password, date_of_birth, lo
 ('Marcos', 'Silva', 'admin@gmail.com', '$2a$12$copXEkXedeIXp6YlzGYrE.yRqPGDIg0j2v.rbBDur9tM.OkfYU.1m', '1990-05-15', false, true);
 
 INSERT INTO app_users_roles (app_user_id, roles_id) VALUES
-(1, 2);
+(1, 1);
